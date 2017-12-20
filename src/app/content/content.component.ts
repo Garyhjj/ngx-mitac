23dd3445c646fb6b9c1a08f1f774c08f5ca2a6d9
Garyhjj <379751452@qq.com>
2017-12-20 22:38:50 +0800

    improve bread

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,6 +1,7 @@
-import { myStore } from './../core/store';
+import { myStore, BreadcrumbState } from './../core/store';
 import { Store } from '@ngrx/store';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-content',
@@ -8,12 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-  
+  breadcrumb: Observable<BreadcrumbState[]>;
   constructor(
     private store$: Store<myStore>
   ) { }
 
   ngOnInit() {
+    this.breadcrumb = this.store$.select((s: myStore) => s.breadcrumbReducer);
   }
 
 }
