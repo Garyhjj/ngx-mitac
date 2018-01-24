adc2b784cb976d60ec747fbf98447a289d59d94a
Garyhjj <379751452@qq.com>
2018-01-24 09:36:02 +0800

    add toExcel fn

--- src/app/core/services/util.service.ts
+++ src/app/core/services/util.service.ts
@@ -1,6 +1,7 @@
 import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { NzMessageService } from 'ng-zorro-antd';
+import * as ExportJsonExcel from 'js-export-excel';
 
 @Injectable()
 export class UtilService {
@@ -16,6 +17,10 @@ export class UtilService {
                 case 403:
                     this.tokenTimeOut();
                     return;
+                case 400:
+                    return this._message.error(err.body, { nzDuration: 3000 });
+                case 500:
+                    return this._message.error('無法連接到服務器,請稍後重試！', { nzDuration: 3000 });
 
             }
         }
@@ -24,6 +29,21 @@ export class UtilService {
     tokenTimeOut() {
         this._message.create('error', '授权已超时,请重新登录', { nzDuration: 5000 });
         this.router.navigate(['/login']);
+    }
+
+    toExcel(name: string, header: (string | number)[], data: (string | number)[][]) {
+        let option: any = {};
+
+        option.fileName = name;
+        option.datas = [
+            {
+                sheetData: data,
+                sheetHeader: header
+            }
+        ];
+        const toExcel = new ExportJsonExcel(option); //new
+        toExcel.saveExcel(); //保存
+        this._message.info('文件將被下載到瀏覽器的默認下載目錄中', { nzDuration: 4000 })
     }
 
 }
--- src/app/shared/components/data-drive/shared/config/index.ts
+++ src/app/shared/components/data-drive/shared/config/index.ts
@@ -7,7 +7,8 @@ export const DataDriveStore = {
             fullScreen: true,
             changeBodyFontSize: true,
             changeHeaderFontSize: true,
-            menu: true
+            menu: true,
+            toExcel: true
         },
         dataViewSet: {
             title: '急料看板',
@@ -94,7 +95,8 @@ export const DataDriveStore = {
             fullScreen: true,
             changeBodyFontSize: true,
             changeHeaderFontSize: true,
-            menu: true
+            menu: true,
+            toExcel: true
         },
         dataViewSet: {
             title: '急料工單看板',
