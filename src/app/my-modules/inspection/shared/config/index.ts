e029b49171f854bd96563cd96413a317b909e863
Garyhjj <379751452@qq.com>
2018-03-08 17:21:50 +0800

    begin inspection-checkList page

--- src/app/my-modules/inspection/shared/config/index.ts
+++ src/app/my-modules/inspection/shared/config/index.ts
@@ -1,4 +1,5 @@
 import { APPConfig } from './../../../../shared/config/app.config';
 export const inspectionConfig = {
-    uploadReport: APPConfig.baseUrl + 'IPQA/UploadReport'
+    uploadReport: APPConfig.baseUrl + 'IPQA/UploadReport',
+    getMRIName: APPConfig.baseUrl + 'IPQA/GetMRIName?company_name={*COMPANY_ID}&type={type}'
 }
